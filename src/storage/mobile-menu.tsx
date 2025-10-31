48cf63cec30885f4b7a6c4ca68549db91e9c0c8c
Daniil <127053306+Lyguh@users.noreply.github.com>
2025-11-01 00:03:14 +0300

    Update mobile-menu.tsx

--- src/storage/mobile-menu.tsx
+++ src/storage/mobile-menu.tsx
@@ -1,8 +1,8 @@
 "use client"
 import React, {useEffect} from 'react';
 import Link from "next/link";
-import {MENU_DATA} from "@/data/menu.data";
 import {cn} from "@/lib/utils";
+import {NAVIGATION_ARRAY} from "@/arrays/navigation.array";
 
 interface IMobileMenuProps {
     isOpen: boolean;
@@ -23,13 +23,12 @@ const MobileMenu = ({ isOpen, onClose }: IMobileMenuProps) => {
     return (
         <>
             <div onClick={onClose} className={cn("fixed inset-0 z-40 bg-black/30 backdrop-blur-md transition-opacity duration-200", isOpen ? "opacity-100 visible" : "opacity-0 invisible" )} />
-            <aside className={cn("fixed top-0 right-0 bottom-0 p-5 max-w-xs w-full h-screen bg-zinc-900 border-l border-zinc-700 z-50 transform transition-transform", isOpen ? "translate-x-0" : "translate-x-full")}>
+            <aside className={cn("fixed top-0 right-0 bottom-0 p-5 max-w-xs w-full h-screen bg-[#000000]/50 border-l border-zinc-700 z-50 transform transition-transform", isOpen ? "translate-x-0" : "translate-x-full")}>
                 <div className="w-full space-y-4">
-                    <Search />
                     <menu className="w-full">
-                        {MENU_DATA.map((item, index) => (
+                        {NAVIGATION_ARRAY.map((item, index) => (
                             <li key={index} className="w-full">
-                                <Link onClick={onClose} href={item.href} className="p-3 inline-block w-full text-lg bg-transparent rounded-md transition-colors hover:bg-zinc-800 hover:text-white">{item.label}</Link>
+                                <Link onClick={onClose} href={item.href} className="p-3 inline-block w-full text-lg bg-transparent rounded-md transition-colors text-[#FFF1EB] hover:bg-zinc-800 hover:text-white">{item.label}</Link>
                             </li>
                         ))}
                     </menu>
