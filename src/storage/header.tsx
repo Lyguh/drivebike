48630906fa5f71adccd6a52146231681dda0f066
Daniil <127053306+Lyguh@users.noreply.github.com>
2025-11-01 00:11:07 +0300

    Update header.tsx

--- src/storage/header.tsx
+++ src/storage/header.tsx
@@ -1,13 +1,16 @@
 "use client"
-import React from 'react';
+import React, {useState} from 'react';
 import Container from "@/components/shared/container";
 import Navigation from "@/storage/navigation";
+import MobileMenu from "@/storage/mobile-menu";
 
 const Header = () => {
+const [menuOpen, setMenuOpen] = useState(false);
     return (
         <header className={"w-full bg-[#000000]/20"} id="header">
             <Container className="w-full">
-                <Navigation className=" flex justify-center"/>
+                <Navigation onOpenMenu={() => setMenuOpen(true)} className=" flex justify-between lg:justify-center"/>
+                <MobileMenu isOpen={menuOpen} onClose={ () => setMenuOpen(false)} />
             </Container>
         </header>
     );
